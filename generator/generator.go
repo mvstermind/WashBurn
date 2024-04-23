133269ffc907633f32c32dbbe5f4e9a6893c53ff
mvstermind <zablockidawid1@gmail.com>
2024-04-23 11:30:52 +0200

    RANDOM KEY AND SCALE IS CHOOOOOOOOSSSSSSEEEEEEEEEEEN

--- generator/generator.go
+++ generator/generator.go
@@ -7,9 +7,7 @@ import (
 
 type Gen struct {
 	Bpm    int
-	Key    []string
-	Scale  []string
-	WScale map[string]string
+	Key    map[int]string
 	Chords [7]string
 }
 
@@ -18,9 +16,33 @@ func (b *Gen) GetBpm() int {
 	return b.Bpm
 }
 
+func (b *Gen) GetKey() string {
+
+	b.Key = map[int]string{
+		1: "C", 2: "C#", 3: "D", 4: "D#", 5: "E", 6: "F", 7: "F#", 8: "G", 9: "G#",
+		10: "A", 11: "A#", 12: "B",
+	}
+	key := rand.Intn(12) + 1
+	gnedKey := b.Key[key]
+
+	scale := [2]string{
+		"Minor", "Major",
+	}
+
+	s := rand.Intn(2)
+	gnedScale := scale[s]
+
+	output := fmt.Sprintf("%v %v\n", gnedKey, gnedScale)
+
+	return output
+}
+
 func main() {
-	gned := Gen{} // Bpm is updated
+	gned := Gen{}
 	bpm := gned.GetBpm()
-	fmt.Printf("Get Bpm %v", bpm)
+	fmt.Printf("Bpm: %v\n", bpm)
+
+	key := gned.GetKey()
+	fmt.Printf("Scale: %v", key)
 
 }
